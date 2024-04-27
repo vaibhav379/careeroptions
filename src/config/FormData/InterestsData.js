a9cad81f78b6161c390192040347d343f7b0b5b4
vaibhav malhotra <vaibhavmalhotra17@gmail.com>
2024-04-28 02:53:16 +0530

    dev changes

--- src/config/FormData/InterestsData.js
+++ src/config/FormData/InterestsData.js
@@ -35,328 +35,328 @@ export const interestsSection = [
       },
     ],
   },
-  {
-    sectionId: 2,
-    description:
-      "When you have free time, how do you typically choose to spend it?",
-    questions: [
-      {
-        questionId: "2_1",
-        question: "Reading books, articles, or blogs on various topics",
-        type: 1,
-      },
-      {
-        questionId: "2_2",
-        question: "Watching movies, documentaries, or online videos",
-        type: 1,
-      },
-      {
-        questionId: "2_3",
-        question:
-          "Engaging in physical activities like going to the gym, yoga, or cycling",
-        type: 1,
-      },
-      {
-        questionId: "2_4",
-        question:
-          "Exploring new places, traveling, or trying out new restaurants",
-        type: 1,
-      },
-      {
-        questionId: "2_5",
-        question:
-          "Relaxing at home, listening to music, or practicing mindfulness and meditation",
-        type: 1,
-      },
-    ],
-  },
-  {
-    sectionId: 3,
-    description:
-      "Are there any specific subjects or topics that you enjoy learning about or discussing?",
-    questions: [
-      {
-        questionId: "3_1",
-        question:
-          "Science and technology, including biology, astronomy, or computer science",
-        type: 1,
-      },
-      {
-        questionId: "3_2",
-        question: "History, anthropology, or cultural studies",
-        type: 1,
-      },
-      {
-        questionId: "3_3",
-        question: "Psychology, sociology, or human behavior",
-        type: 1,
-      },
-      {
-        questionId: "3_4",
-        question: "Business, economics, or entrepreneurship",
-        type: 1,
-      },
-      {
-        questionId: "3_5",
-        question: "Environmental studies, sustainability, or ecology",
-        type: 1,
-      },
-    ],
-  },
-  {
-    sectionId: 4,
-    description:
-      "What types of tasks or projects do you excel in or feel most confident completing?",
-    questions: [
-      {
-        questionId: "4_1",
-        question: " Organizing and planning events, schedules, or projects",
-        type: 1,
-      },
-      {
-        questionId: "4_2",
-        question:
-          "Problem-solving and troubleshooting technical issues or complex problems",
-        type: 1,
-      },
-      {
-        questionId: "4_3",
-        question: "Writing, editing, or communicating ideas effectively",
-        type: 1,
-      },
-      {
-        questionId: "4_4",
-        question: "Leading and motivating teams or groups toward a common goal",
-        type: 1,
-      },
-      {
-        questionId: "4_5",
-        question:
-          "Researching, analyzing data, and drawing insights or conclusions",
-        type: 1,
-      },
-    ],
-  },
-  {
-    sectionId: 5,
-    description:
-      "Think about a time when you were completely absorbed in an activity. What were you doing?",
-    questions: [
-      {
-        questionId: "5_1",
-        question:
-          "Designing or building something from scratch, like a piece of furniture or a website",
-        type: 1,
-      },
-      {
-        questionId: "5_2",
-        question:
-          "Solving a challenging problem or puzzle, like a Rubik's cube or a Sudoku",
-        type: 1,
-      },
-      {
-        questionId: "5_3",
-        question:
-          "Engaging in a deep conversation or debate with friends or colleagues",
-        type: 1,
-      },
-      {
-        questionId: "5_4",
-        question:
-          "Participating in a competitive sport or game that required focus and strategy",
-        type: 1,
-      },
-      {
-        questionId: "5_5",
-        question:
-          "Immersing yourself in a creative endeavor, like writing a story or composing music",
-        type: 1,
-      },
-    ],
-  },
-  {
-    sectionId: 6,
-    description:
-      "Do you prefer working independently or as part of a team? Why?",
-    questions: [
-      {
-        questionId: "6_1",
-        question:
-          "I prefer working independently because I can set my own pace and focus without distractions.",
-        type: 1,
-      },
-      {
-        questionId: "6_2",
-        question:
-          " I enjoy working in small teams where I can collaborate closely with others and bounce ideas off each other.",
-        type: 1,
-      },
-      {
-        questionId: "6_3",
-        question:
-          "I thrive in large team environments where there's diversity of thought and ample opportunity for brainstorming.",
-        type: 1,
-      },
-      {
-        questionId: "6_4",
-        question:
-          "I like a balance of both independent work and teamwork, depending on the task at hand.",
-        type: 1,
-      },
-      {
-        questionId: "6_5",
-        question:
-          "I don't have a strong preference; I adapt my working style based on the situation and requirements.",
-        type: 1,
-      },
-    ],
-  },
-  {
-    sectionId: 7,
-    description:
-      "What kinds of environments or settings do you feel most comfortable and productive in?",
-    questions: [
-      {
-        questionId: "7_1",
-        question: "A quiet and organized workspace with minimal distractions",
-        type: 1,
-      },
-      {
-        questionId: "7_2",
-        question:
-          "An open and collaborative office environment with plenty of interaction",
-        type: 1,
-      },
-      {
-        questionId: "7_3",
-        question:
-          "A dynamic and fast-paced environment where things are constantly changing",
-        type: 1,
-      },
-      {
-        questionId: "7_4",
-        question:
-          "A remote or virtual setting where I can work from anywhere with flexibility",
-        type: 1,
-      },
-      {
-        questionId: "7_5",
-        question:
-          "A structured environment with clear expectations and routines",
-        type: 1,
-      },
-    ],
-  },
-  {
-    sectionId: 8,
-    description:
-      "Reflect on past experiences. What aspects of previous jobs, internships, or volunteer opportunities did you enjoy the most?",
-    questions: [
-      {
-        questionId: "8_1",
-        question:
-          " Interacting with customers, clients, or stakeholders and providing excellent service",
-        type: 1,
-      },
-      {
-        questionId: "8_2",
-        question:
-          "Being given autonomy and freedom to take ownership of projects or initiatives",
-        type: 1,
-      },
-      {
-        questionId: "8_3",
-        question:
-          "Learning new skills or technologies and applying them to real-world situations",
-        type: 1,
-      },
-      {
-        questionId: "8_4",
-        question:
-          "Collaborating with colleagues or team members to achieve common goals",
-        type: 1,
-      },
-      {
-        questionId: "8_5",
-        question:
-          "Making a positive impact on the community, environment, or society through meaningful work",
-        type: 1,
-      },
-    ],
-  },
-  {
-    sectionId: 9,
-    description:
-      "Are there any role models or individuals whose careers you admire or aspire to emulate?",
-    questions: [
-      {
-        questionId: "9_1",
-        question:
-          "Elon Musk, for his innovation and entrepreneurial spirit in multiple industries",
-        type: 1,
-      },
-      {
-        questionId: "9_2",
-        question:
-          "Michelle Obama, for her advocacy work and commitment to education and social issues",
-        type: 1,
-      },
-      {
-        questionId: "9_3",
-        question:
-          "Stephen Hawking, for his groundbreaking contributions to theoretical physics and cosmology",
-        type: 1,
-      },
-      {
-        questionId: "9_4",
-        question:
-          "Oprah Winfrey, for her media empire and philanthropic endeavors",
-        type: 1,
-      },
-      {
-        questionId: "9_5",
-        question:
-          "Malala Yousafzai, for her activism and advocacy for girls' education and human rights",
-        type: 1,
-      },
-    ],
-  },
-  {
-    sectionId: 10,
-    description:
-      "If money were not a factor, how would you choose to spend your time in terms of work or activities?",
-    questions: [
-      {
-        questionId: "10_1",
-        question:
-          "Pursuing a passion project or hobby full-time, such as writing, painting, or starting a nonprofit",
-        type: 1,
-      },
-      {
-        questionId: "10_2",
-        question:
-          "Traveling the world and experiencing different cultures, languages, and cuisines",
-        type: 1,
-      },
-      {
-        questionId: "10_3",
-        question:
-          "Investing in further education or personal development to continuously learn and grow",
-        type: 1,
-      },
-      {
-        questionId: "10_4",
-        question:
-          "Volunteering or giving back to the community through charitable work or mentoring",
-        type: 1,
-      },
-      {
-        questionId: "10_5",
-        question:
-          "Spending quality time with loved ones and focusing on personal well-being and fulfillment",
-        type: 1,
-      },
-    ],
-  },
+  // {
+  //   sectionId: 2,
+  //   description:
+  //     "When you have free time, how do you typically choose to spend it?",
+  //   questions: [
+  //     {
+  //       questionId: "2_1",
+  //       question: "Reading books, articles, or blogs on various topics",
+  //       type: 1,
+  //     },
+  //     {
+  //       questionId: "2_2",
+  //       question: "Watching movies, documentaries, or online videos",
+  //       type: 1,
+  //     },
+  //     {
+  //       questionId: "2_3",
+  //       question:
+  //         "Engaging in physical activities like going to the gym, yoga, or cycling",
+  //       type: 1,
+  //     },
+  //     {
+  //       questionId: "2_4",
+  //       question:
+  //         "Exploring new places, traveling, or trying out new restaurants",
+  //       type: 1,
+  //     },
+  //     {
+  //       questionId: "2_5",
+  //       question:
+  //         "Relaxing at home, listening to music, or practicing mindfulness and meditation",
+  //       type: 1,
+  //     },
+  //   ],
+  // },
+  // {
+  //   sectionId: 3,
+  //   description:
+  //     "Are there any specific subjects or topics that you enjoy learning about or discussing?",
+  //   questions: [
+  //     {
+  //       questionId: "3_1",
+  //       question:
+  //         "Science and technology, including biology, astronomy, or computer science",
+  //       type: 1,
+  //     },
+  //     {
+  //       questionId: "3_2",
+  //       question: "History, anthropology, or cultural studies",
+  //       type: 1,
+  //     },
+  //     {
+  //       questionId: "3_3",
+  //       question: "Psychology, sociology, or human behavior",
+  //       type: 1,
+  //     },
+  //     {
+  //       questionId: "3_4",
+  //       question: "Business, economics, or entrepreneurship",
+  //       type: 1,
+  //     },
+  //     {
+  //       questionId: "3_5",
+  //       question: "Environmental studies, sustainability, or ecology",
+  //       type: 1,
+  //     },
+  //   ],
+  // },
+  // {
+  //   sectionId: 4,
+  //   description:
+  //     "What types of tasks or projects do you excel in or feel most confident completing?",
+  //   questions: [
+  //     {
+  //       questionId: "4_1",
+  //       question: " Organizing and planning events, schedules, or projects",
+  //       type: 1,
+  //     },
+  //     {
+  //       questionId: "4_2",
+  //       question:
+  //         "Problem-solving and troubleshooting technical issues or complex problems",
+  //       type: 1,
+  //     },
+  //     {
+  //       questionId: "4_3",
+  //       question: "Writing, editing, or communicating ideas effectively",
+  //       type: 1,
+  //     },
+  //     {
+  //       questionId: "4_4",
+  //       question: "Leading and motivating teams or groups toward a common goal",
+  //       type: 1,
+  //     },
+  //     {
+  //       questionId: "4_5",
+  //       question:
+  //         "Researching, analyzing data, and drawing insights or conclusions",
+  //       type: 1,
+  //     },
+  //   ],
+  // },
+  // {
+  //   sectionId: 5,
+  //   description:
+  //     "Think about a time when you were completely absorbed in an activity. What were you doing?",
+  //   questions: [
+  //     {
+  //       questionId: "5_1",
+  //       question:
+  //         "Designing or building something from scratch, like a piece of furniture or a website",
+  //       type: 1,
+  //     },
+  //     {
+  //       questionId: "5_2",
+  //       question:
+  //         "Solving a challenging problem or puzzle, like a Rubik's cube or a Sudoku",
+  //       type: 1,
+  //     },
+  //     {
+  //       questionId: "5_3",
+  //       question:
+  //         "Engaging in a deep conversation or debate with friends or colleagues",
+  //       type: 1,
+  //     },
+  //     {
+  //       questionId: "5_4",
+  //       question:
+  //         "Participating in a competitive sport or game that required focus and strategy",
+  //       type: 1,
+  //     },
+  //     {
+  //       questionId: "5_5",
+  //       question:
+  //         "Immersing yourself in a creative endeavor, like writing a story or composing music",
+  //       type: 1,
+  //     },
+  //   ],
+  // },
+  // {
+  //   sectionId: 6,
+  //   description:
+  //     "Do you prefer working independently or as part of a team? Why?",
+  //   questions: [
+  //     {
+  //       questionId: "6_1",
+  //       question:
+  //         "I prefer working independently because I can set my own pace and focus without distractions.",
+  //       type: 1,
+  //     },
+  //     {
+  //       questionId: "6_2",
+  //       question:
+  //         " I enjoy working in small teams where I can collaborate closely with others and bounce ideas off each other.",
+  //       type: 1,
+  //     },
+  //     {
+  //       questionId: "6_3",
+  //       question:
+  //         "I thrive in large team environments where there's diversity of thought and ample opportunity for brainstorming.",
+  //       type: 1,
+  //     },
+  //     {
+  //       questionId: "6_4",
+  //       question:
+  //         "I like a balance of both independent work and teamwork, depending on the task at hand.",
+  //       type: 1,
+  //     },
+  //     {
+  //       questionId: "6_5",
+  //       question:
+  //         "I don't have a strong preference; I adapt my working style based on the situation and requirements.",
+  //       type: 1,
+  //     },
+  //   ],
+  // },
+  // {
+  //   sectionId: 7,
+  //   description:
+  //     "What kinds of environments or settings do you feel most comfortable and productive in?",
+  //   questions: [
+  //     {
+  //       questionId: "7_1",
+  //       question: "A quiet and organized workspace with minimal distractions",
+  //       type: 1,
+  //     },
+  //     {
+  //       questionId: "7_2",
+  //       question:
+  //         "An open and collaborative office environment with plenty of interaction",
+  //       type: 1,
+  //     },
+  //     {
+  //       questionId: "7_3",
+  //       question:
+  //         "A dynamic and fast-paced environment where things are constantly changing",
+  //       type: 1,
+  //     },
+  //     {
+  //       questionId: "7_4",
+  //       question:
+  //         "A remote or virtual setting where I can work from anywhere with flexibility",
+  //       type: 1,
+  //     },
+  //     {
+  //       questionId: "7_5",
+  //       question:
+  //         "A structured environment with clear expectations and routines",
+  //       type: 1,
+  //     },
+  //   ],
+  // },
+  // {
+  //   sectionId: 8,
+  //   description:
+  //     "Reflect on past experiences. What aspects of previous jobs, internships, or volunteer opportunities did you enjoy the most?",
+  //   questions: [
+  //     {
+  //       questionId: "8_1",
+  //       question:
+  //         " Interacting with customers, clients, or stakeholders and providing excellent service",
+  //       type: 1,
+  //     },
+  //     {
+  //       questionId: "8_2",
+  //       question:
+  //         "Being given autonomy and freedom to take ownership of projects or initiatives",
+  //       type: 1,
+  //     },
+  //     {
+  //       questionId: "8_3",
+  //       question:
+  //         "Learning new skills or technologies and applying them to real-world situations",
+  //       type: 1,
+  //     },
+  //     {
+  //       questionId: "8_4",
+  //       question:
+  //         "Collaborating with colleagues or team members to achieve common goals",
+  //       type: 1,
+  //     },
+  //     {
+  //       questionId: "8_5",
+  //       question:
+  //         "Making a positive impact on the community, environment, or society through meaningful work",
+  //       type: 1,
+  //     },
+  //   ],
+  // },
+  // {
+  //   sectionId: 9,
+  //   description:
+  //     "Are there any role models or individuals whose careers you admire or aspire to emulate?",
+  //   questions: [
+  //     {
+  //       questionId: "9_1",
+  //       question:
+  //         "Elon Musk, for his innovation and entrepreneurial spirit in multiple industries",
+  //       type: 1,
+  //     },
+  //     {
+  //       questionId: "9_2",
+  //       question:
+  //         "Michelle Obama, for her advocacy work and commitment to education and social issues",
+  //       type: 1,
+  //     },
+  //     {
+  //       questionId: "9_3",
+  //       question:
+  //         "Stephen Hawking, for his groundbreaking contributions to theoretical physics and cosmology",
+  //       type: 1,
+  //     },
+  //     {
+  //       questionId: "9_4",
+  //       question:
+  //         "Oprah Winfrey, for her media empire and philanthropic endeavors",
+  //       type: 1,
+  //     },
+  //     {
+  //       questionId: "9_5",
+  //       question:
+  //         "Malala Yousafzai, for her activism and advocacy for girls' education and human rights",
+  //       type: 1,
+  //     },
+  //   ],
+  // },
+  // {
+  //   sectionId: 10,
+  //   description:
+  //     "If money were not a factor, how would you choose to spend your time in terms of work or activities?",
+  //   questions: [
+  //     {
+  //       questionId: "10_1",
+  //       question:
+  //         "Pursuing a passion project or hobby full-time, such as writing, painting, or starting a nonprofit",
+  //       type: 1,
+  //     },
+  //     {
+  //       questionId: "10_2",
+  //       question:
+  //         "Traveling the world and experiencing different cultures, languages, and cuisines",
+  //       type: 1,
+  //     },
+  //     {
+  //       questionId: "10_3",
+  //       question:
+  //         "Investing in further education or personal development to continuously learn and grow",
+  //       type: 1,
+  //     },
+  //     {
+  //       questionId: "10_4",
+  //       question:
+  //         "Volunteering or giving back to the community through charitable work or mentoring",
+  //       type: 1,
+  //     },
+  //     {
+  //       questionId: "10_5",
+  //       question:
+  //         "Spending quality time with loved ones and focusing on personal well-being and fulfillment",
+  //       type: 1,
+  //     },
+  //   ],
+  // },
 ];
 
--- src/config/FormData/SocioEconomicData.js
+++ src/config/FormData/SocioEconomicData.js
@@ -20,88 +20,88 @@ export const socioEconomicSections = [
         },
       ],
     },
-    {
-      sectionId: 21,
-      description: "What is the primary source of income for your household?",
-      questions: [
-        {
-          questionId: "21_1",
-          question: "Both parents are employed in professional careers with stable incomes.",
-          type: 1,
-        },
-        {
-          questionId: "21_2",
-          question: "One parent is employed in a stable job, while the other earns income through irregular or informal work.",
-          type: 1,
-        },
-        {
-          questionId: "21_3",
-          question: "Household income primarily relies on agricultural activities or informal labor, with fluctuating earnings.",
-          type: 1,
-        },
-      ],
-    },
-    {
-      sectionId: 22,
-      description: "How would you describe the living conditions in your neighborhood?",
-      questions: [
-        {
-          questionId: "22_1",
-          question: "We live in a well-developed urban area with access to good infrastructure, healthcare, and educational institutions.",
-          type: 1,
-        },
-        {
-          questionId: "22_2",
-          question: "Our neighborhood is a mix of urban and rural elements, with basic amenities but limited access to quality services.",
-          type: 1,
-        },
-        {
-          questionId: "22_3",
-          question: "We live in a rural or semi-urban area with limited infrastructure and services, including irregular access to electricity, water, and transportation.",
-          type: 1,
-        },
-      ],
-    },
-    {
-      sectionId: 23,
-      description: "Have you faced any financial constraints or difficulties in pursuing your education or career goals?",
-      questions: [
-        {
-          questionId: "23_1",
-          question: "No, my family has been able to support me financially in pursuing my education and career aspirations without significant constraints.",
-          type: 1,
-        },
-        {
-          questionId: "23_2",
-          question: "Yes, I have faced occasional financial challenges, but my family has managed to overcome them through sacrifices and careful budgeting.",
-          type: 1,
-        },
-        {
-          questionId: "23_3",
-          question: "Yes, financial constraints have significantly impacted my education and career choices, forcing me to compromise or delay my goals.",
-          type: 1,
-        },
-      ],
-    },
-    {
-      sectionId: 24,
-      description: "How do you perceive the availability of job opportunities in your locality or city?",
-      questions: [
-        {
-          questionId: "24_1",
-          question: "There are ample job opportunities in various sectors, with options for professional growth and advancement.",
-          type: 1,
-        },
-        {
-          questionId: "24_2",
-          question: "Job opportunities are limited, especially in specific industries or sectors, leading to high competition and challenges in finding suitable employment.",
-          type: 1,
-        },
-        {
-          questionId: "24_3",
-          question: "Job opportunities are scarce, particularly in rural areas, leading to high unemployment rates and migration to urban centers in search of work.",
-          type: 1,
-        },
-      ],
-    },
+    // {
+    //   sectionId: 21,
+    //   description: "What is the primary source of income for your household?",
+    //   questions: [
+    //     {
+    //       questionId: "21_1",
+    //       question: "Both parents are employed in professional careers with stable incomes.",
+    //       type: 1,
+    //     },
+    //     {
+    //       questionId: "21_2",
+    //       question: "One parent is employed in a stable job, while the other earns income through irregular or informal work.",
+    //       type: 1,
+    //     },
+    //     {
+    //       questionId: "21_3",
+    //       question: "Household income primarily relies on agricultural activities or informal labor, with fluctuating earnings.",
+    //       type: 1,
+    //     },
+    //   ],
+    // },
+    // {
+    //   sectionId: 22,
+    //   description: "How would you describe the living conditions in your neighborhood?",
+    //   questions: [
+    //     {
+    //       questionId: "22_1",
+    //       question: "We live in a well-developed urban area with access to good infrastructure, healthcare, and educational institutions.",
+    //       type: 1,
+    //     },
+    //     {
+    //       questionId: "22_2",
+    //       question: "Our neighborhood is a mix of urban and rural elements, with basic amenities but limited access to quality services.",
+    //       type: 1,
+    //     },
+    //     {
+    //       questionId: "22_3",
+    //       question: "We live in a rural or semi-urban area with limited infrastructure and services, including irregular access to electricity, water, and transportation.",
+    //       type: 1,
+    //     },
+    //   ],
+    // },
+    // {
+    //   sectionId: 23,
+    //   description: "Have you faced any financial constraints or difficulties in pursuing your education or career goals?",
+    //   questions: [
+    //     {
+    //       questionId: "23_1",
+    //       question: "No, my family has been able to support me financially in pursuing my education and career aspirations without significant constraints.",
+    //       type: 1,
+    //     },
+    //     {
+    //       questionId: "23_2",
+    //       question: "Yes, I have faced occasional financial challenges, but my family has managed to overcome them through sacrifices and careful budgeting.",
+    //       type: 1,
+    //     },
+    //     {
+    //       questionId: "23_3",
+    //       question: "Yes, financial constraints have significantly impacted my education and career choices, forcing me to compromise or delay my goals.",
+    //       type: 1,
+    //     },
+    //   ],
+    // },
+    // {
+    //   sectionId: 24,
+    //   description: "How do you perceive the availability of job opportunities in your locality or city?",
+    //   questions: [
+    //     {
+    //       questionId: "24_1",
+    //       question: "There are ample job opportunities in various sectors, with options for professional growth and advancement.",
+    //       type: 1,
+    //     },
+    //     {
+    //       questionId: "24_2",
+    //       question: "Job opportunities are limited, especially in specific industries or sectors, leading to high competition and challenges in finding suitable employment.",
+    //       type: 1,
+    //     },
+    //     {
+    //       questionId: "24_3",
+    //       question: "Job opportunities are scarce, particularly in rural areas, leading to high unemployment rates and migration to urban centers in search of work.",
+    //       type: 1,
+    //     },
+    //   ],
+    // },
   ];
